4cfce7250091987d152d02304771b9799ae485c8
Janith Randeniya <jrandeniya@gmail.com>
2017-12-19 22:42:43 +1100

    Fix whole number parsing NA decimals

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function thousandSep(val) {
 	const parts = String(val).split('.');
 	const part1 = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
 	const part2 = parts[1];
-	return (part1 + '.' + part2);
+	return `${part1}${part2 ? '.' + part2 : ''}`
 }
 
 app.set('view engine', 'ejs');
